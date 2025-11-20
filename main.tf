abb083efd5746e06961dfc15259d8eef08d3806c
mukeshdevelp <msmukeshkumarsharma95@gmail.com>
2025-11-20 18:51:25 +0530

    main file added

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "sg" {
     ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "HTTPS"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
