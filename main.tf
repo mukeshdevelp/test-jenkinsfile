c69a4d6c28a0511e3c7772e65744111cb9a492b3
mukeshdevelp <msmukeshkumarsharma95@gmail.com>
2025-11-20 18:46:43 +0530

    http

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "sg" {
     ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "tcp"
+    protocol    = "HTTPS"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
